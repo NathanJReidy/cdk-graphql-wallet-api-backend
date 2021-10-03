9ac11ae96843081531d040e9a64bbb5d0b3a2732
Nathan Reidy <nathanjreidy@gmail.com>
2021-10-03 22:42:49 +1100

    Configured basic appsync api stack

--- lib/appsync-cdk-app-stack.ts
+++ lib/appsync-cdk-app-stack.ts
@@ -1,9 +1,40 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from "@aws-cdk/core";
+import * as appsync from "@aws-cdk/aws-appsync";
+import * as ddb from "@aws-cdk/aws-dynamodb";
+import * as lambda from "@aws-cdk/aws-lambda";
 
 export class AppsyncCdkAppStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Create the AppSync API
+    const api = new appsync.GraphqlApi(this, "Api", {
+      name: "cdk-wallet-appsync-api",
+      schema: appsync.Schema.fromAsset("graphql/schema.graphql"),
+      authorizationConfig: {
+        defaultAuthorization: {
+          authorizationType: appsync.AuthorizationType.API_KEY,
+          apiKeyConfig: {
+            expires: cdk.Expiration.after(cdk.Duration.days(365)),
+          },
+        },
+      },
+      xrayEnabled: true,
+    });
+
+    // Prints out the AppSync GraphQL API key to the terminal
+    new cdk.CfnOutput(this, "GraphQLAPIURL", {
+      value: api.graphqlUrl,
+    });
+
+    // Prints out the AppSync GraphQL API key to the terminal
+    new cdk.CfnOutput(this, "GraphQLAPIKey", {
+      value: api.apiKey || "",
+    });
+
+    // Prints out the stack region to the terminal
+    new cdk.CfnOutput(this, "Stack Region", {
+      value: this.region,
+    });
   }
 }
